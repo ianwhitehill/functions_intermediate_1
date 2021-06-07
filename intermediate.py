2b71594b5dab77a939564e1a1140ed465370f9d1
ianwhitehill <smwirbw@gmail.com>
2021-06-07 17:07:23 -0500

    first commit

---
+++ intermediate.py
@@ -0,0 +1,80 @@
+#1
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0] = 15
+# print(x)
+
+students[0]["last_name"] = "Bryant"
+# print(students)
+
+sports_directory["soccer"][0] = "Andres"
+# print(sports_directory)
+
+#2
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+# def iterateDictionary(list):
+#     for i in range(0, len(list)):
+#         for k, v in list[i].items():
+#             print(f"{k} - {v}")
+
+# iterateDictionary(students) 
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+#3
+def iterateDictionary2(key_name, some_list):
+    for v in some_list:
+        print(v[key_name])
+        continue
+
+iterateDictionary2('first_name', students)
+
+#4
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+def printInfo(item):
+    for k in item:
+        print(f"{len(item[k])} {k}")
+        for v in range(0, len(item[k])):
+            print(item[k][v])
+
+printInfo(dojo)
+# output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+    
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
